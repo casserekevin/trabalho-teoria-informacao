751a6f1c6031dc8fc6da00afad5d271e5c95bd5f
brunoschubert <brunoschubertm@gmail.com>
2018-09-29 23:27:39 -0300

    Correção do LZW Descompactador e adição ao main

--- CompressAlgoritm/src/Coders/LzwComp.java
+++ CompressAlgoritm/src/Coders/LzwComp.java
@@ -4,120 +4,114 @@ import java.io.*;
 import java.util.*;
 
 public class LzwComp {
-	  // define a hashmap e outras variaveis que serão utilizadas pelo programa
+//----------------------------------------------------------------------------------------------------//
+//--------------------------------------VARIÁVEIS-----------------------------------------------------//
+//----------------------------------------------------------------------------------------------------//
+// define a hashmap e outras variaveis que serão utilizadas pelo programa
     public HashMap<String, Integer> dictionary = new HashMap<>();
     public int dictSize = 256;
     public String str = "";
     public byte inputByte;
     public byte[] buffer = new byte[3];
     public boolean onleft = true;
+//----------------------------------------------------------------------------------------------------//
+//----------------------------------MÉTODOS DE COMPRESSÃO---------------------------------------------//
+//----------------------------------------------------------------------------------------------------//
+//Recebe um arquivo para comprimir. Ao final adiciona .lzw como extensão
+      public void compress(String uncompressed) throws IOException {
+          // Dictionary size limit, builds dictionary
+          for (int i = 0; i < 256; i++) {
+              dictionary.put(Character.toString((char) i), i);
+          }
 
-    //Recebe um arquivo para comprimir
-    public void compress(String uncompressed) throws IOException {
-        // Dictionary size limit, builds dictionary
-        for (int i = 0; i < 256; i++) {
-            dictionary.put(Character.toString((char) i), i);
-        }
+          //Lê o impute uncompr e escreve na compr 
+          RandomAccessFile read = new RandomAccessFile(uncompressed, "r");
+          RandomAccessFile out = new RandomAccessFile(uncompressed.concat(
+                  ".lzw"), "rw");
 
-        //Lê o impute uncompr e escreve na compr 
-        RandomAccessFile read = new RandomAccessFile(uncompressed, "r");
-        RandomAccessFile out = new RandomAccessFile(uncompressed.concat(
-                ".lzw"), "rw");
+          try {
+              // lê  o primeiro caractere do imput em uma String
+              inputByte = read.readByte();
+              int i = new Byte(inputByte).intValue();
+              if (i < 0) {
+                  i += 256;
+              }
+              char ch = (char) i;
+              str = "" + ch;
 
-        try {
-            // lê  o primeiro caractere do imput em uma String
-            inputByte = read.readByte();
-            int i = new Byte(inputByte).intValue();
-            if (i < 0) {
-                i += 256;
-            }
-            char ch = (char) i;
-            str = "" + ch;
+              // Lê caractere por caractere
+              while (true) {
+                  inputByte = read.readByte();
+                  i = new Byte(inputByte).intValue();
 
-            // Lê caractere por caractere
-            while (true) {
-                inputByte = read.readByte();
-                i = new Byte(inputByte).intValue();
+                  if (i < 0) {
+                      i += 256;
+                  }
+                  System.out.print(i + ", ");
+                  ch = (char) i;
 
-                if (i < 0) {
-                    i += 256;
-                }
-                System.out.print(i + ", ");
-                ch = (char) i;
+                  //Se 	str + ch est]ao no dicionário
+                  // Set str to str + ch
+                  if (dictionary.containsKey(str + ch)) {
+                      str = str + ch;
+                  } else {
+                      String s12 = to12bit(dictionary.get(str));
 
-                //Se 	str + ch est]ao no dicionário
-                // Set str to str + ch
-                if (dictionary.containsKey(str + ch)) {
-                    str = str + ch;
-                } else {
-                    String s12 = to12bit(dictionary.get(str));
+                      // Coloca os 12 bit sem um array e então esceve iusto no arquivo output
+                      
+                      if (onleft) {
+                          buffer[0] = (byte) Integer.parseInt(
+                                  s12.substring(0, 8), 2);
+                          buffer[1] = (byte) Integer.parseInt(
+                                  s12.substring(8, 12) + "0000", 2);
+                      } else {
+                          buffer[1] += (byte) Integer.parseInt(
+                                  s12.substring(0, 4), 2);
+                          buffer[2] = (byte) Integer.parseInt(
+                                  s12.substring(4, 12), 2);
+                          for (int b = 0; b < buffer.length; b++) {
+                              out.writeByte(buffer[b]);
+                              buffer[b] = 0;
+                          }
+                      }
+                      onleft = !onleft;
 
-                    // Coloca os 12 bit sem um array e então esceve iusto no arquivo output
-                    
-                    if (onleft) {
-                        buffer[0] = (byte) Integer.parseInt(
-                                s12.substring(0, 8), 2);
-                        buffer[1] = (byte) Integer.parseInt(
-                                s12.substring(8, 12) + "0000", 2);
-                    } else {
-                        buffer[1] += (byte) Integer.parseInt(
-                                s12.substring(0, 4), 2);
-                        buffer[2] = (byte) Integer.parseInt(
-                                s12.substring(4, 12), 2);
-                        for (int b = 0; b < buffer.length; b++) {
-                            out.writeByte(buffer[b]);
-                            buffer[b] = 0;
-                        }
-                    }
-                    onleft = !onleft;
+                      // adiciona str + ch ao dicionário
+                      if (dictSize < 4096) {
+                          dictionary.put(str + ch, dictSize++);
+                      }
 
-                    // adiciona str + ch ao dicionário
-                    if (dictSize < 4096) {
-                        dictionary.put(str + ch, dictSize++);
-                    }
+                      // Set str to ch
+                      str = "" + ch;
+                  }
+              }
+              
+          } catch (IOException e) {
+              String str12bit = to12bit(dictionary.get(str));
+              if (onleft) {
+                  buffer[0] = (byte) Integer.parseInt(str12bit.substring(0, 8), 2);
+                  buffer[1] = (byte) Integer.parseInt(str12bit.substring(8, 12)
+                          + "0000", 2);
+                  out.writeByte(buffer[0]);
+                  out.writeByte(buffer[1]);
+              } else {
+                  buffer[1] += (byte) Integer.parseInt(str12bit.substring(0, 4), 2);
+                  buffer[2] = (byte) Integer.parseInt(str12bit.substring(4, 12), 2);
 
-                    // Set str to ch
-                    str = "" + ch;
-                }
-            }
-            
-        } catch (IOException e) {
-            String str12bit = to12bit(dictionary.get(str));
-            if (onleft) {
-                buffer[0] = (byte) Integer.parseInt(str12bit.substring(0, 8), 2);
-                buffer[1] = (byte) Integer.parseInt(str12bit.substring(8, 12)
-                        + "0000", 2);
-                out.writeByte(buffer[0]);
-                out.writeByte(buffer[1]);
-            } else {
-                buffer[1] += (byte) Integer.parseInt(str12bit.substring(0, 4), 2);
-                buffer[2] = (byte) Integer.parseInt(str12bit.substring(4, 12), 2);
-
-                for (int b = 0; b < buffer.length; b++) {
-                    out.writeByte(buffer[b]);
-                    buffer[b] = 0;
-                }
-            }
-            read.close();
-            out.close();
-        }
-    }
-
-    /**
-     * Converte 8 bits para 12 bits
-     *
-     * @param i - Integer value
-     * @return - String value of integer in 12 bit
-     */
-    public String to12bit(int i) {
-        String str = Integer.toBinaryString(i);
-        while (str.length() < 12) {
-            str = "0" + str;
-        }
-        return str;
-    }
-    
-    
-    
-    
-}
+                  for (int b = 0; b < buffer.length; b++) {
+                      out.writeByte(buffer[b]);
+                      buffer[b] = 0;
+                  }
+              }
+              read.close();
+              out.close();
+          }
+      }
+      public String to12bit(int i) {
+          String str = Integer.toBinaryString(i);
+          while (str.length() < 12) {
+              str = "0" + str;
+          }
+          return str;
+      }
+  }
--- CompressAlgoritm/src/Main/Main.java
+++ CompressAlgoritm/src/Main/Main.java
@@ -5,15 +5,21 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-import Coders.LzwComp;
 //import Coders.RLE;
+import Coders.LzwComp;
+//import Coders.LzwDec;
+
 //import FileOperators.FileOpener;
 //import Manager.ExecutionManager;
+
+
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-
+//-------------------------------------------------------------------------------------------------------//
+//-------------------------------------RLE COMPACTADOR---------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------//
 		/*
 		RLE rle = new RLE();
 		ExecutionManager executionManager = new ExecutionManager(rle);
@@ -21,13 +27,15 @@ public class Main {
 		executionManager.encodar(FileOpener.TXT_E_EXE);
 //		executionManager.decodar();
 		 */
+//-------------------------------------------------------------------------------------------------------//
 //-------------------------------------LZW COMPACTADOR---------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------//
 		try {
 			LzwComp lzw = new LzwComp();
 
 			Scanner input = new Scanner(System.in);
 
-			System.out.println("Coloque o nome do (input.txt) arquivo.");
+			System.out.println("Adicione a localização e o nome do arquivo.");
 
 			String str = input.nextLine();
 
@@ -50,10 +58,37 @@ public class Main {
 		} catch (FileNotFoundException e) {
 			System.out.println("Arquivo não encontrado!");
 		}
+//------------------------------------------------------------------------------------------------------//		
+//-------------------------------------------LZW DESCOMPACTADOR-----------------------------------------//
+//------------------------------------------------------------------------------------------------------//
+/*
+		try {
+			LzwDec lzw = new LzwDec();
 
-		//-------------------------------------LZW DESCOMPACTADOR-----------------------------------------//
+			Scanner input = new Scanner(System.in);
 
-		
+			System.out.println("Enter the name of your (input.txt.lzw) file.");
+
+			String str = input.nextLine();
+
+			File file = new File(str);
+
+			Scanner fileScanner = new Scanner(file);
+
+			String line = "";
+
+			while (fileScanner.hasNext()) {
+				line = fileScanner.nextLine();
+				System.out.println("Contents of your file being decompressed:\n"
+						+ line);
+			}
+			lzw.LZW_Decompress(str);
+			System.out.println("Decompression of your file is complete!");
+			System.out.println("Your new file is named: "
+					+ str.replace(".lzw", ""));
+		} catch (FileNotFoundException e) {
+			System.out.println("File was not found!");
+		}
+*/
 	}
-
 }
